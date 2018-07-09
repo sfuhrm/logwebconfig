7f59e25df8d65fb059951589d7c9178980374211
Stephan Fuhrmann <s@sfuhrm.de>
2018-07-09 19:05:50 +0200

    Control flow improvement

--- logwebconfig/src/main/java/de/sfuhrm/logwebconfig/Server.java
+++ logwebconfig/src/main/java/de/sfuhrm/logwebconfig/Server.java
@@ -26,84 +26,69 @@ final class Server extends NanoHTTPD {
 
     @Override
     public Response serve(final IHTTPSession session) {
-
         try {
             Method method = session.getMethod();
             switch (method) {
                 case PUT:
-                    return configure(session);
+                    configure(session);
+                    break;
                 default:
-                    return methodNotAllowed(method);
+                    throw new ServerException(
+                            Response.Status.METHOD_NOT_ALLOWED,
+                            method.toString());
             }
-        } catch (IOException | ResponseException e) {
-            return newFixedLengthResponse(
-                    Response.Status.INTERNAL_ERROR,
-                    NanoHTTPD.MIME_PLAINTEXT,
-                    e.getMessage());
-        }
-    }
-
-    /** Configures a logger based on the data in the request.
-     * @param session the session to take the parameters from.
-     * @return the response to return to the HTTP client.
-     * @throws IOException if reading the files fails.
-     * @throws ResponseException if reading the files fails.
-     * */
-    private Response configure(final IHTTPSession session)
-            throws IOException, ResponseException {
-        String logger = session.getUri();
-        Map<String, String> params = session.getParms();
-        String levelString = params.get(PARAM_LEVEL);
-        if (levelString == null) {
-            return badRequest("Parameter '" + PARAM_LEVEL + "' is missing\r\n");
-        }
-
-        if (logger.startsWith("/")) {
-            logger = logger.substring(1);
-        }
-
-        LogConfigurator logConfigurator = new Log4j2Configurator();
-        Optional<LogConfigurator.Resource> resource =
-                logConfigurator.findResource(logger);
-        if (!resource.isPresent()) {
-            return newFixedLengthResponse(
-                    Response.Status.NOT_FOUND,
-                    MIME_PLAINTEXT,
-                    "Not found: " + logger + "\r\n");
-        }
-
-        try {
-            if (resource.isPresent()) {
-                resource.get().update(levelString);
-            }
-        } catch (IllegalArgumentException e) {
-            return badRequest(e.getMessage());
+        } catch (ServerException e) {
+            return e.toResponse();
         }
         return newFixedLengthResponse(
                 Response.Status.OK,
                 MIME_PLAINTEXT,
-                "OK\r\n");
+                "");
     }
 
-    /** Returns a bad request HTTP response.
-     * @param message the plain text message to pass along with the response.
-     * @return the created response object for this error.
-     * */
-    private Response badRequest(final String message) {
-        return newFixedLengthResponse(
-                Response.Status.BAD_REQUEST,
-                MIME_PLAINTEXT,
-                message);
+    /** Get the logger resource associated with the request.
+     * @param session the session to get the resource from.
+     * @return the logger resource associated with the request.
+     * @throws ServerException if the logger was not found.
+     *  */
+    private LogConfigurator.Resource getResource(
+            final IHTTPSession session) throws ServerException {
+        String logger = session.getUri();
+        if (logger.startsWith("/")) {
+            logger = logger.substring(1);
+        }
+        LogConfigurator logConfigurator = new Log4j2Configurator();
+        Optional<LogConfigurator.Resource> resource =
+                logConfigurator.findResource(logger);
+        if (!resource.isPresent()) {
+            throw new ServerException(Response.Status.NOT_FOUND,
+                    "Logger not found: " + logger + "'");
+        }
+        return resource.get();
     }
 
-    /** Returns a method not allowed HTTP response.
-     * @param method the method that was used in the request.
-     * @return the created response object for this error.
+    /** Configures a logger based on the data in the request.
+     * @param session the session to take the parameters from.
+     * @throws ServerException if the request is malformed.
      * */
-    private Response methodNotAllowed(final Method method) {
-        return newFixedLengthResponse(
-                Response.Status.METHOD_NOT_ALLOWED,
-                MIME_PLAINTEXT,
-                "Method " + method + " is not allowed\r\n");
+    private void configure(final IHTTPSession session)
+            throws ServerException {
+        String logger = session.getUri();
+        Map<String, String> params = session.getParms();
+        String levelString = params.get(PARAM_LEVEL);
+        if (levelString == null) {
+            throw new ServerException(Response.Status.BAD_REQUEST,
+                    "Parameter '" + PARAM_LEVEL + "' is missing");
+        }
+
+        LogConfigurator.Resource resource =
+                getResource(session);
+
+        try {
+                resource.update(levelString);
+        } catch (IllegalArgumentException e) {
+            throw new ServerException(Response.Status.BAD_REQUEST,
+                    e.getMessage());
+        }
     }
 }
